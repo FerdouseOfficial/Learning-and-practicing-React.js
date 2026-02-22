2373adc01f1911a6426658ee09fbe9ea25ec4e7f
FerdouseOfficial <mollajihad402@gmail.com>
2026-02-23 00:27:15 +0600

    I have created a card.jsx file

--- 02-folder/src/App.jsx
+++ 02-folder/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Card from './components/card'
+import Navbar  from './components/Navbar'
 
 const App = () => {
   return (
     <div>
+      <Navbar/>
       <Card />
     </div>
   )
