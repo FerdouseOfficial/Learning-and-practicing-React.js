5a8eb4b8ad9e4fd6d9030ab66e6897b6423a3a43
FerdouseOfficial <mollajihad402@gmail.com>
2026-02-22 23:24:49 +0600

    finished the first process

--- O1-folder/src/App.jsx
+++ O1-folder/src/App.jsx
@@ -1,6 +1,17 @@
-function App() {
-  return 'hello';
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className="dad">
+        <h1 className="child1">Child One</h1>
+        <h2 className="child2">Child Two</h2>
+      </div>
+      <div className="chacha">
+
+      </div>
+    </div>
+  )
 }
 
-App();
-<App />
+export default App
