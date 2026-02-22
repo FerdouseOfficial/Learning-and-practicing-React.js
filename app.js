fa328cf1e970f340df14999819d8fd7bb16fe9b4
FerdouseOfficial <mollajihad402@gmail.com>
2026-02-22 22:37:47 +0600

    I have write the full descrioption of the export types

--- app.js
+++ app.js
@@ -3,3 +3,14 @@ let a = 10;
 export default a;
 
 export let messi = "Goat";
+
+/**
+ * Actually import and export divided into 2 part okay 
+ *  1 is defaulth export 
+ *      inside of default export we just write export then defaulth then the var name and call it where we want it with different names or what ever we want okay
+ *  2 is named export
+ *      but in name export we need to write export just before the var
+ *      then if we want to access it into other file we have to call it this way    import {the exact var name} from the file name this way. okay
+ * 
+ * I hope you under it
+ */
