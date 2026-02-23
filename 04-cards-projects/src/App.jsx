5afc7a9fcc1f6c1c5715111f248da0450417442b
FerdouseOfficial <mollajihad402@gmail.com>
2026-02-23 14:14:42 +0600

    I have created a file inside the src/ assests foldere and name it as images to store all the images that will be used inside the website okay

--- 04-cards-projects/src/App.jsx
+++ 04-cards-projects/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Cards from './components/cards'
 
 const App = () => {
   return (
     <div className='parent'>
-
+      <Cards />
     </div>
   )
 }
---
+++ 04-cards-projects/src/components/cards.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Cards = () => {
+  return (
+    <div className='cards'>
+        <div className="top">
+            <img src="" alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default Cards
