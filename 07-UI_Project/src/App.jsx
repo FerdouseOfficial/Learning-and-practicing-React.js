73d45c8647c0d63768d59a64e27c594ed3288ecf
FerdouseOfficial <mollajihad402@gmail.com>
2026-03-04 15:37:24 +0600

    I have created a file inside the compoent/Section1 folder okay

--- 07-UI_Project/src/App.jsx
+++ 07-UI_Project/src/App.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <div className='bg-amber-700 h-full w-full '>
-      <h1 className='p-4'>My name is What?</h1>
+    <div className="bg-amber-700 h-full w-full ">
+      <h1 className="p-4">My name is What?</h1>
       <button class="bg-blue-500 text-white px-4 py-2">Click me</button>
     </div>
-  )
-}
+  );
+};
 
-export default App 
+export default App;
