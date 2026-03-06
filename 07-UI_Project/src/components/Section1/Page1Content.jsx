536d51ba20ae8c1f4a184c4bb2deae8bf266bfc0
FerdouseOfficial <mollajihad402@gmail.com>
2026-03-07 02:16:01 +0600

    I have cerated 2 file lef and right conetnt.jsx file for the content part in the cardokay

---
+++ 07-UI_Project/src/components/Section1/Page1Content.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Page1Content = () => {
+  return (
+    <div className="py-3 px-18 flex justify-">
+      <h2 className="text-3xl ">Prospective customer segmentation</h2>
+      <p>
+        Depending on customer satisfaction and access to banking products,potential target audience can be divided into three groups
+      </p>
+    </div>
+  );
+};
+
+export default Page1Content;
--- 07-UI_Project/src/components/Section1/Section1.jsx
+++ 07-UI_Project/src/components/Section1/Section1.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './Navbar'
+import Page1Content from './Page1Content'
 
 const Section1 = () => {
   return (
     <div className='bg-red-900 h-screen w-full'>
         <h2>Section1</h2>
         <Navbar/>
+        <Page1Content/>
     </div>
   )
 }
