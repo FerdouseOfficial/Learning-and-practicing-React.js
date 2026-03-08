80f08a0ccc008ca46c964ac21a90c280c5dbb53e
FerdouseOfficial <mollajihad402@gmail.com>
2026-03-08 09:08:24 +0600

    I have finished practicing the concept

--- 08-functions/src/App.jsx
+++ 08-functions/src/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
 
 const App = () => {
+  function btnclicked(){
+    console.log("button is clicked");
+  }
+  // function inputChanging(val) {
+  //   console.log(val);
+  // }
+  const pageScrolling= (val) =>{
+    if(val>0){
+      console.log("Going forward");
+    } else{
+      console.log("Going backward")
+    }
+  }
   return (
-    <div>App</div>
+    <div onWheel={(elem) =>{
+      pageScrolling(elem.deltaY);
+    }}>
+      <div className="page1"></div>
+      <div className="page2"></div>
+      <div className="page3"></div>
+    </div>
   )
 }
 
