3d52a006e3175d5e113f13db46a5d0dd806b4101
FerdouseOfficial <mollajihad402@gmail.com>
2026-03-09 23:04:24 +0600

    I have practiced the concept of useEffect okay

--- 16-useEffect/src/App.jsx
+++ 16-useEffect/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  useEffect(function () {
+    console.log("use effect is running");
+  }, [num]);
+  const [num, setnum] = useState(0);
+  const [num2, setNum2] = useState(0);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h1>Num {num}</h1>
+      <h1>Num {num2}</h1>
+      <button
+        onMouseOver={() => {
+          setnum(num + 1);
+        }}
+        onMouseLeave={() => {
+          setNum2(num2+10)
+        }}
+      >
+        hover
+      </button>
+    </div>
+  );
+};
 
-export default App
+export default App;
