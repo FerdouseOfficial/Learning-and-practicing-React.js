cb75e71f8b6e07ad0427ec9af080a89448147055
FerdouseOfficial <mollajihad402@gmail.com>
2026-03-11 12:42:07 +0600

    Create new file for new feature okay

--- 19-Advanced-routing/src/Pages/About.jsx
+++ 19-Advanced-routing/src/Pages/About.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 const About = () => {
+  // let data = useParams()
+  let navigate = useNavigate();
+
   return (
     <div>
-      <button className="bg-black text-white text-xl px-8 py-2 rounded-2xl ml-2 cursor-pointer active:scale-95">
+      <button
+        onClick={() => {
+          navigate('/')
+        }}
+        className="bg-black text-white text-xl px-8 py-2 rounded-2xl ml-2 cursor-pointer active:scale-95"
+      >
         Return to Home Page
+      </button>
+      <button
+        onClick={() => {
+          navigate(-1)
+        }}
+        className="bg-black text-white text-xl px-8 py-2 rounded-2xl ml-2 cursor-pointer active:scale-95"
+      >
+        back
       </button>
       <div className="max-w-2xl mx-auto px-4 py-12">
         <h1 className="text-4xl font-bold mb-6 text-gray-900">About Us</h1>
