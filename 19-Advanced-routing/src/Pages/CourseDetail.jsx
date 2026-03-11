b128541c719d391de9ff523c17818eb40aa2b0f1
FerdouseOfficial <mollajihad402@gmail.com>
2026-03-11 12:14:44 +0600

    I have 2 more files CourseDetails.jsx Courses.jsx file to practice the concept of Dynamic Routing okay

---
+++ 19-Advanced-routing/src/Pages/CourseDetail.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CourseDetail = () => {
+  return (
+    <div>
+      <h3>They you are. Congrats</h3>
+      <p>Now go fuck yourself</p>
+    </div>
+  );
+};
+
+export default CourseDetail;
