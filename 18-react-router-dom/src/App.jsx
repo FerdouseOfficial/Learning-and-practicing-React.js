5eeaccb55db340d462aa3031bfe32c9cf1b8868b
FerdouseOfficial <mollajihad402@gmail.com>
2026-03-11 08:24:50 +0600

    I am facing some problem but no matter what I will fix it and come back okay bababe

--- 18-react-router-dom/src/App.jsx
+++ 18-react-router-dom/src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import About from './component/About'
+import Home from './component/Home'
 
 const App = () => {
+  console.log("Fuck off you little brat")
   return (
     <div>
-      <Route>
-        
-      </Route>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+      </Routes>
     </div>
   )
 }
--- 18-react-router-dom/src/component/Home.jsx
+++ 18-react-router-dom/src/component/Home.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return <div>
+    <h1 className="bg-red-500 text-white">Fuck off</h1>
+  </div>;
 };
 
 export default Home;
