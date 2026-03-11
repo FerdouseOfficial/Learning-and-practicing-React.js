fcacbdf120903f16208e831adf51d20914b27d51
FerdouseOfficial <mollajihad402@gmail.com>
2026-03-11 06:26:06 +0600

    started practicing the concepts okay

--- 18-react-router-dom/src/App.jsx
+++ 18-react-router-dom/src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div className=''>App</div>
+    <div>
+      <Route>
+        
+      </Route>
+    </div>
   )
 }
 
