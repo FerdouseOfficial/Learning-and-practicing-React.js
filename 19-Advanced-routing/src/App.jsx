2d23701eb3c1134dada1eee339a2f75ee43b13ef
FerdouseOfficial <mollajihad402@gmail.com>
2026-03-11 10:29:50 +0600

    I have done it okay

--- 19-Advanced-routing/src/App.jsx
+++ 19-Advanced-routing/src/App.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -6,6 +5,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Product from "./pages/Product";
+import NotFound from "./pages/NotFound";
 
 const app = () => {
   return (
@@ -16,6 +16,7 @@ const app = () => {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/product" element={<Product />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
     </div>
